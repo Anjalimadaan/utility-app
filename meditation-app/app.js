3403d69f8fab5e39b441b3bd381309b1059ef947
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-05-01 17:53:45 +0530

    funcionality of the meditation app

---
+++ meditation-app/app.js
@@ -0,0 +1,82 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    // Sounds
+    const sounds = document.querySelectorAll('.sound-select button');
+
+    // Time display
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
+
+    // Get the length of the outline 
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+
+    // Pick different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        })
+    })
+
+    // Duration
+    let duration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // Play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    })
+
+    // Select time
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() {
+            duration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(duration / 60)}:${Math.floor(duration % 60)}`;
+        })
+    });
+
+    // A function to check if the song is playing
+    const checkPlaying = song => {
+        if(song.paused) {
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg'
+        }
+    }
+
+    // Animate the circle
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = duration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // Animation
+        let progress = outlineLength - (currentTime / duration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        // Animate the text
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if (currentTime >= duration) {
+            song.currentTime = 0;
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    }
+}
+
+app();
