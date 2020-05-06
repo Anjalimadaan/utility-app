8a8248a5785613c6b960faba4234f0563a12b588
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-05-06 11:49:22 +0530

    completed calculator

--- calculator/app.js
+++ calculator/app.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     }
 
     appendNumber(number) {
@@ -21,19 +21,66 @@ class Calculator {
     }
 
     chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
         this.operation = operation;
         this.previousOperand = this.currentOperand;
         this.currentOperand = '';
-        
     }
 
-    compute() {
+   compute() {
+       let computation;
+       const previous = parseFloat(this.previousOperand);
+       const current = parseFloat(this.currentOperand);
+       if (isNaN(previous) || isNaN(current)) return;
+       switch (this.operation) {
+           case '+':
+               computation = previous + current;
+               break;
+            case '-':
+               computation = previous - current;
+               break;
+            case '×':
+               computation = previous * current;
+               break;
+            case '÷':
+               computation = previous / current;
+               break;
+            default:
+               return;
+       }
+       this.currentOperand = computation;
+       this.operation = undefined;
+       this.previousOperand = '';
+    }
 
+    getDisplay(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits : 0});
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
     }
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand;
-        this.previousOperandTextElement.innerText = this.previousOperand;
+        this.currentOperandTextElement.innerText = this.getDisplay(this.currentOperand);
+        if (this.operation != undefined) {
+            this.previousOperandTextElement.innerText = `${this.getDisplay(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+        
     }
 }
 
@@ -64,3 +111,17 @@ operationButtons.forEach(button => {
     });
 });
 
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay()
+});
